aa4d8649a78d865ad1d9b9905ca1bae20c1b32c2
Данил Исламов <d.islamov@mail.business.ru>
2025-07-22 10:58:12 +0500

    refactor: rename package

--- lib/bru_dart_linter.dart
+++ lib/bru_dart_linter.dart
@@ -1,7 +1,7 @@
 import 'package:custom_lint_builder/custom_lint_builder.dart';
 
-import 'package:bru_linter_dart/src/missing_comma_lint.dart';
-import 'package:bru_linter_dart/src/missing_type_lint.dart';
+import 'package:bru_dart_linter/src/missing_comma_lint.dart';
+import 'package:bru_dart_linter/src/missing_type_lint.dart';
 
 PluginBase createPlugin() => _BruLinter();
 
